3b85d423738b88b616ff9eb8527d5bae2de317b3
criss.gomez1992@hotmail.com <criss.gomez1992@hotmail.com>
2023-07-26 21:03:10 +0000

    first commit test

--- notebooks/01_data_exploration.py
+++ notebooks/01_data_exploration.py
@@ -10,3 +10,8 @@
 
 # MAGIC %sql
 # MAGIC SELECT * FROM json.`dbfs:/mnt/azure-data-lake/amazon_reviews/partition_1/part-00000-tid-9136122565017344171-3f98196e-e0c5-4bb5-90cc-d523170ef713-86080-1-c000.json.gz`
+
+# COMMAND ----------
+
+# MAGIC %md 
+# MAGIC # hola
