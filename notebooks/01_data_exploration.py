7af40ed44c40ff203339c4821189861c708a104b
criss.gomez1992@hotmail.com <criss.gomez1992@hotmail.com>
2023-07-28 01:26:36 +0000

    first analysis with the data in amazon_reviews
    creating the dataframes from the bronze schema and calculating the percentage of null values in each colum

--- notebooks/01_data_exploration.py
+++ notebooks/01_data_exploration.py
@@ -1,17 +1,69 @@
 # Databricks notebook source
 # MAGIC %md
-# MAGIC #This is the EDA of the project
+# MAGIC #Exploratory Data Analysis
+# MAGIC
 
 # COMMAND ----------
 
-# MAGIC %fs ls mnt/azure-data-lake/amazon_reviews/partition_1/
+from pyspark.sql import DataFrame
+from pyspark.sql.functions import col, count, when
 
 # COMMAND ----------
 
-# MAGIC %sql
-# MAGIC SELECT * FROM json.`dbfs:/mnt/azure-data-lake/amazon_reviews/partition_1/part-00000-tid-9136122565017344171-3f98196e-e0c5-4bb5-90cc-d523170ef713-86080-1-c000.json.gz`
+# MAGIC %md
+# MAGIC ### Dataframes
 
 # COMMAND ----------
 
-# MAGIC %md 
-# MAGIC # hola
+# Read the data from the Gold schema
+df_reviews = spark.table("bronze.amazon_reviews")
+df_metadata = spark.table("bronze.amazon_metadata")
+
+# COMMAND ----------
+
+total_rows_reviews = df_reviews.count()
+total_rows_metadata = df_metadata.count()
+print("Number of rows in the DataFrame of amazon reviews: ", total_rows_reviews)
+print("Number of rows in the DataFrame of amazon metadata: ", total_rows_metadata)
+
+# COMMAND ----------
+
+display(df_reviews)
+
+# COMMAND ----------
+
+display(df_metadata)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC #### Null values
+
+# COMMAND ----------
+
+def calculate_nulls(df: DataFrame) -> DataFrame:
+    """
+    Calculate the percentage of null values for each column in a Spark DataFrame.
+
+    Args:
+        df (DataFrame): The Spark DataFrame.
+
+    Returns:
+        DataFrame: A DataFrame containing the percentage of null values for each column in the input DataFrame.
+    """
+    # total number of rows in the DataFrame
+    total_rows = df.count()
+
+    # count of null values for each column
+    null_counts = df.select([count(when(col(c).isNull(), c)).alias(c) for c in df.columns])
+
+    # percentage of null values for each column
+    null_percentages = null_counts.select([((col(c) / total_rows) * 100).alias(f"{c}_null_percentage") for c in df.columns])
+
+    return null_percentages
+
+
+# COMMAND ----------
+
+null_reviews=calculate_nulls(df_reviews)
+display(null_reviews)
